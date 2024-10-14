7352a617d69b3de08b8754a29c6a74b8cda572f8
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-14 10:51:07 +0200

    Implemented game logic and binded with UI

--- Right On Target/ViewController.swift
+++ Right On Target/ViewController.swift
@@ -11,12 +11,45 @@ class ViewController: UIViewController {
     
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+    var round: Int = 0
+    var points: Int = 0
+    
+    @IBAction func checkNumber() {
+        if self.round == 0 {
+            self.number = Int.random(in: 1...50)
+            self.label.text = String(self.number)
+            self.round = 1
+        } else {
+            let numSlider = Int(self.slider.value.rounded())
+            
+            if numSlider > self.number {
+                self.points += 50 - numSlider + self.number
+            } else if numSlider < self.number {
+                self.points += 50 - self.number + numSlider
+            } else {
+                self.points += 50
+            }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            if self.round == 5 {
+                let alert = UIAlertController(
+                    title: "Game Over!",
+                    message: "You earned \(self.points) points",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Restart", style:
+                        .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                self.round = 1
+                self.points = 0
+            } else {
+                self.round += 1
+            }
+            
+            self.number = Int.random(in: 1...50)
+            
+            self.label.text = String(self.number)
+        }
     }
-
-
 }
 
