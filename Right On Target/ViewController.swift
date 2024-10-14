083d8757ad4df18eb63f49e59a8a0bab2eaefaf9
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-14 09:28:47 +0200

    Initial Commit

---
+++ Right On Target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right On Target
+//
+//  Created by Nikita Shyshkin on 14/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
