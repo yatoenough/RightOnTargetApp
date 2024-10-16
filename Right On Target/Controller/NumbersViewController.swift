7faae9a56d56a6f69da084f85b51711e7510a7fd
Nikita Shyshkin <yatoenough@gmail.com>
2024-10-16 22:41:55 +0200

    Removed unneccesary code, added number game VC

---
+++ Right On Target/Controller/NumbersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NumbersViewController.swift
+//  Right On Target
+//
+//  Created by Nikita Shyshkin on 16/10/2024.
+//
+
+import UIKit
+
+class NumbersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
